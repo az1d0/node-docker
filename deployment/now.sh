4ade7da8a83ac4893ae831f0ac287dcc91fafbdb
Manel <manel.marcos@gmail.com>
2018-04-16 01:10:29 +0200

    Fix deploy to now

--- deployment/now.sh
+++ deployment/now.sh
@@ -1,5 +1,6 @@
 npm install -g now
 echo "Deploying in now..."
-URL = $(now --public --docker -t $NOW_TOKEN)
-echo "Running on $URL"
-curl --silent -L $URL
+# URL = $(now --public --docker -t $NOW_TOKEN)
+now --public --docker -t $NOW_TOKEN
+echo "Acabao"
+# curl --silent -L $URL
